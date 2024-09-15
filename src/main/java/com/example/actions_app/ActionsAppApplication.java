a8e627a49cb076227003360a5be638856959b1d3
Parag Gawde <paraggawde@Parags-Laptop.local>
2024-09-15 14:54:17 +0530

    Initial commit

---
+++ src/main/java/com/example/actions_app/ActionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.actions_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actions_app/ActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actions_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
